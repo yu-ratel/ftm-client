81b61863a6f090a613df0b6060b877ddf7624bfc
yu-ratel <dbdudals100@gmail.com>
2025-02-16 16:28:17 +0900

    chore: init library

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+import QueryProvider from "./Provider";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
+    </html>
+  );
+}
