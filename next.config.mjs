94bd697f1fda96fde63b3b6b30091c86e45c655b
dongmin3891 <rlaehdals753@naver.com>
2025-04-01 01:07:03 +0900

    chore: 크로스 도메인 요청 설정 임시 주석처리

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  // 크로스 도메인 요청 허용
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: `${process.env.NEXT_PUBLIC_API_URL}/api/users/:path*`,
+  //     },
+  //   ];
+  // },
+};
 
 export default nextConfig;
